b2b3a0a95074e9d51ade0fd6b2876ae504dc6e2e
UzielSantana <uzzsantana@gmail.com>
2023-05-09 13:33:42 -0300

    primeiro commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,200 @@
+<template>
+
+<div>
+  
+    <div class="chart">
+      <div class="wrapper">
+    <div class="sidebar">
+      <!-- conteúdo da barra lateral -->
+    </div>
+    <div class="main-content">
+      <!-- conteúdo principal -->
+    </div>
+  </div>
+      <canvas id="myChart"></canvas>
+    </div>
+  </div>
+  
+<div>
+  <div class>   </div> 
+  
+<br>
+  <h1> Essas são as metas individuais de destravamentos no Onboarding Enterprise</h1>
+ <div class="grafico1"> <canvas id="myChart"></canvas> </div>
+
+    <h1 class="h12">Destravamentos - Ativados - Travados - Desaparecidos </h1>
+ <div> <canvas id="myChart2"></canvas> </div> 
+</div>
+
+
+  <div>
+    <div class="sidebar">
+      <Sidebar @update-chart="updateChart"></Sidebar>
+    </div>
+    <div class="chart">
+      <canvas id="myChart"></canvas>
+    </div>
+    <br>
+    <h1>Essas são as metas individuais de destravamentos no Onboarding Enterprise</h1>
+    <div class="grafico1">
+      <canvas id="myChart2"></canvas>
+    </div>
+    <h1 class="h12">Destravamentos - Ativados - Travados - Desaparecidos </h1>
+    <div>
+      <canvas id="myChart3"></canvas>
+    </div>
+  </div>
+</template>
+<script>
+import Chart from 'chart.js/auto';
+import Sidebar from './SideBar.vue'
+
+export default {
+  components: {
+    Sidebar,
+  },
+  mounted() {
+    this.drawChart();
+  },
+  methods: {
+    drawChart() {
+      const ctx = document.getElementById('myChart');
+      this.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Bia', 'Carol', 'Dani', 'Nath', 'Thay', 'Uzi'],
+          datasets: [
+            {
+              label: 'Meta Realizada',
+              data: [1, 5, 1, 3, 3, 2, 5],
+              backgroundColor: '#FF6384', // cor da barra para 'Meta Realizada'
+              borderWidth: 1,
+              width: 20,
+            },
+            {
+              label: 'Meta Não Realizada',
+              data: [4, 2, 4, 2, 2, 3, 4],
+              backgroundColor: '#36A2EB', // cor da barra para 'Meta Não Realizada'
+              borderWidth: 1,
+              width: 20,
+            },
+         
+            /*
+            {
+              label: 'Meta Parcialmente Realizada',
+              data: [2, 3, 3, 1, 3, 3, 3],
+              backgroundColor: '#FFCE56', // cor da barra para 'Meta Parcialmente Realizada'
+              borderWidth: 1,
+              width: 20,
+            }, */
+          ] 
+        },
+        options: {
+          layout: {
+            padding: 90
+          },
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    },
+    updateChart(data) {
+      // Atualize o gráfico com base nos dados recebidos da barra lateral
+      this.chart.data.datasets[0].data[0] = data.data1;
+      this.chart.data.datasets[1].data[0] = data.data2;
+      this.chart.data.datasets[0].data[1] = data.data3;
+      this.chart.data.datasets[1].data[1] = data.data4;
+      this.chart.data.datasets[0].data[2] = data.data5;
+      this.chart.data.datasets[1].data[2] = data.data6;
+   
+
+ 
+      this.chart.update();
+      console.log(data)
+    }
+  }
+};
+</script>
+
+<style >
+
+
+
+.wrapper {
+  display: flex;
+  height: 100%;
+}
+
+.sidebar {
+  margin:0;
+  width: 200px;
+  background-color: #ccc;
+  padding: 20px;
+}
+
+.main-content {
+  flex: 1;
+  background-color: #fff;
+  padding: 20px;
+}
+
+
+
+body {
+
+
+  background-color: rgb(223, 214, 214);
+  
+}
+
+img {
+
+  margin-left: 50px;
+  margin-right: -50px;
+  margin-top: 30px
+  wi
+  
+}
+
+h1 { 
+
+  margin-left: -80px ;
+  margin-top: 40px;
+  margin-bottom:-50px;
+  padding-bottom: - 70px;
+
+  color:rgb(222, 222, 35);
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+
+.h12 { 
+
+margin-left: -80px ;
+margin-top:40px;
+margin-bottom:-50px;
+padding-bottom: - 70px;
+color:rgb(222, 222, 35);
+
+font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+
+.grafico {
+
+  max-width: 200px;
+}
+
+canvas {
+
+  width: 100px;
+}
+
+.imgs {
+
+  display: flex;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+      <h2>Atualizado do gráfico</h2>
+      <form>
+        <label for="data1">Meta Bia:</label>
+        <input type="number" id="data1" v-model="data1">
+        <label for="data2">Meta Atingida Bia :</label>
+        <input type="number" id="data2" v-model="data2">
+        <label for="data3">Meta Carol:</label>
+        <input type="number" id="data3" v-model="data3">
+        <label for="data4">Meta Atingida Carol:</label>
+        <input type="number" id="data4" v-model="data4">
+        <label for="data5">Meta Dani:</label>
+        <input type="number" id="data5" v-model="data5">
+        <label for="data6">Meta Atingida Dani:</label>
+        <input type="number" id="data6" v-model="data6">
+        <label for="data7">Meta Nath:</label>
+        <input type="number" id="data7" v-model="data7">
+        <label for="data8">Meta Atingida Nath:</label>
+        <input type="number" id="data8" v-model="data8">
+        <label for="data9">Meta Thay:</label>
+        <input type="number" id="data9" v-model="data9">
+        <label for="data10">Meta Atingida Thay:</label>
+        <input type="number" id="data10" v-model="data10">
+        
+
+        <button type="button" @click="updateChart">Atualizar</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        data1: 0,
+        data2: 0,
+        data3: 0,
+        data4: 0,
+        data5: 0,
+        data6: 0,
+        data7: 0,
+        data8: 0,
+        data9: 0,
+        data10: 0,
+      
+      }
+    },
+    methods: {
+      updateChart() {
+        this.$emit('update-chart', {
+          data1: this.data1,
+          data2: this.data2,
+          data3: this.data3,
+          data4: this.data4,
+          data5: this.data5,
+          data6: this.data6,
+          data7: this.data7,
+          data8: this.data8,
+          data9: this.data9,
+          data10: this.data10,
+
+        })
+      }
+    }
+  }
+  </script>
